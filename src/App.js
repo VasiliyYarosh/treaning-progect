76c29084408f386db67bc6ef99ff43ae45decf39
VasiliyYarosh <vasilliy.yarosh@gmail.com>
2022-06-21 10:04:51 +0300

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Technologies from "./Technologies";
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1> 
+    <div>
+      <Header />
+      <Technologies />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+      <div>
+        <a href='#s'>Home</a>
+        <a href='#s'>News Feed</a>
+        <a href='#s'>Messages</a>
+      </div>
+  )
+}
+export default Header;
